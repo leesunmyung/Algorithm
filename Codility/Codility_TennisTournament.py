181c336c4bdf737bd5da929dccdbec2578ec8151
Sunmyung Lee <vividlee81@sookmyung.ac.kr>
2022-07-03 23:44:01 +0900

    Add files via upload

---
+++ Codility/Codility_TennisTournament.py
@@ -0,0 +1,8 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(P, C):
+    if (P // 2 <= C):
+        return P // 2
+    else: 
+        return C
